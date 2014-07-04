23eb4c7d0f6d0ec0c55510d509dec5f07e0a7c7e
Mitsuyoshi Sakamoto <skmt@cosmicengine.co.jp>
2014-07-04 13:25:07 +0900

    add new cask

---
+++ Casks/snippetsapp.rb
@@ -0,0 +1,9 @@
+class Snippetsapp < Cask
+  version '1.4.4'
+  sha256 '4b4e7807e149bc40bd7d63d4a4676bb14c01924423df6fda544fc4d6957a24ac'
+
+  url 'http://www.snippetsapp.com/download/Snippets-1.4.4.zip'
+  homepage 'http://www.snippetsapp.com/'
+
+  link 'Snippets.app'
+end

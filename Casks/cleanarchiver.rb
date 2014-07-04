545554ba23d6f1c383ab958fe6c3f17beb602fa5
Mitsuyoshi Sakamoto <skmt@cosmicengine.co.jp>
2014-07-05 01:07:57 +0900

    add new cask

---
+++ Casks/cleanarchiver.rb
@@ -0,0 +1,9 @@
+class Cleanarchiver < Cask
+  version '3.0a6'
+  sha256 '9b24c7ac24e976b6d364a81e746dcd1526fcbda57e787cd3c0fce00eba1b5847'
+
+  url 'http://www.sopht.jp/pub/Mac/CleanArchiver-3.0a6.dmg'
+  homepage 'http://www.sopht.jp/cleanarchiver/'
+
+  link 'CleanArchiver.app'
+end

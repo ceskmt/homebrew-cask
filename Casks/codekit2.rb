ef6bd41cfa9a87ab77a38ed62442de217e7f4b07
Mitsuyoshi Sakamoto <skmt@cosmicengine.co.jp>
2014-08-01 11:28:41 +0900

    add new cask

---
+++ Casks/codekit2.rb
@@ -0,0 +1,10 @@
+class Codekit2 < Cask
+  version '2.1.3 (17682)'
+  sha256 '46a868939650b09b7af1a3e84d60c82366f17d7a19df27b300ff9d61bd95cdd8'
+
+  url 'https://incident57.com/codekit/files/codekit-17682.zip'
+  appcast 'https://incident57.com/codekit/appcast/ck2appcast.xml'
+  homepage 'http://incident57.com/codekit/'
+
+  link 'CodeKit.app'
+end

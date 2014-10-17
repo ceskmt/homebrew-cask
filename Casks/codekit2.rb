7e752f65e8f8709018067e2954bd9be81e96e865
Mitsuyoshi Sakamoto <skmt@cosmicengine.co.jp>
2014-10-17 11:11:11 +0900

    edit cask

--- Casks/codekit2.rb
+++ Casks/codekit2.rb
@@ -1,8 +1,8 @@
 class Codekit2 < Cask
-  version '2.1.7 (17945)'
-  sha256 '2d68f834e911527cce1c45315b8d8a986191b3aa0226371585a52ef0c9856c91'
+  version '2.1.8 (18027)'
+  sha256 'b31769ae91834a06499d876c82f2f2b1ce8b379047aa5d89e7d4f50971328a83'
 
-  url 'https://incident57.com/codekit/files/codekit-17945.zip'
+  url 'https://incident57.com/codekit/files/codekit-18027.zip'
   appcast 'https://incident57.com/codekit/appcast/ck2appcast.xml'
   homepage 'http://incident57.com/codekit/'
 

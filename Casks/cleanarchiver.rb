2ec7c44cec649f4906aa9c73911177686e37f12c
Mitsuyoshi Sakamoto <skmt@cosmicengine.co.jp>
2015-02-02 23:05:23 +0900

    edit cask

--- Casks/cleanarchiver.rb
+++ Casks/cleanarchiver.rb
@@ -1,9 +1,10 @@
-class Cleanarchiver < Cask
+cask :v1 => 'cleanarchiver' do
   version '3.0a6'
   sha256 '9b24c7ac24e976b6d364a81e746dcd1526fcbda57e787cd3c0fce00eba1b5847'
 
   url 'http://www.sopht.jp/pub/Mac/CleanArchiver-3.0a6.dmg'
   homepage 'http://www.sopht.jp/cleanarchiver/'
+  license :unknown
 
-  link 'CleanArchiver.app'
+  app 'CleanArchiver.app'
 end
--- Casks/snippetsapp.rb
+++ Casks/snippetsapp.rb
@@ -1,9 +1,10 @@
-class Snippetsapp < Cask
+cask :v1 =>  'snippetsapp' do
   version '1.4.4'
   sha256 '4b4e7807e149bc40bd7d63d4a4676bb14c01924423df6fda544fc4d6957a24ac'
 
   url 'http://www.snippetsapp.com/download/Snippets-1.4.4.zip'
   homepage 'http://www.snippetsapp.com/'
+  license :unknown
 
-  link 'Snippets.app'
+  app 'Snippets.app'
 end
